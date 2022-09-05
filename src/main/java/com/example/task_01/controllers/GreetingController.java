6f9dfc60ca721aab9b98682bc420fd6d4ef24cce
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-05 14:40:52 +0100

    getmapping

--- src/main/java/com/example/task_01/controllers/GreetingController.java
+++ src/main/java/com/example/task_01/controllers/GreetingController.java
@@ -1,7 +1,13 @@
 package com.example.task_01.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    @GetMapping?("/greeting")
+    public String greeting() {
+        return "Good afternoon Shivangi!";
+    }
 }
