1ea93d7c49b999207a11b68a46b2f10c2cee712a
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-05 15:56:07 +0100

    created getters and setters for message

--- src/main/java/com/example/task_01/models/Celebration.java
+++ src/main/java/com/example/task_01/models/Celebration.java
@@ -4,5 +4,15 @@ public class Celebration {
 
     private String message;
 
-    
+    public Celebration(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
