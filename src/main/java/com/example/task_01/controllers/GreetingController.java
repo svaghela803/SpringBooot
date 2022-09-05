ac86489b99e458e246475fcdea49de39aace2e83
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-05 16:05:34 +0100

    added pseudo code

--- src/main/java/com/example/task_01/controllers/GreetingController.java
+++ src/main/java/com/example/task_01/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.task_01.controllers;
 
+import com.example.task_01.models.Celebration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     @GetMapping
+
+//    Extension task 1
     public String newGreeting(@RequestParam(value = "timeOfDay") String timeOfDay) {
         return String.format(timeOfDay + " Shivangi" );
     }
+
+//    http://localhost:8080/greeting?timeOfDay=Afternoon prints out "Good afternoon Shivangi"
+
+
+//    public Celebration newGreeting(){
+//        Greeting greeting = new Greeting("Afternoon");
+//        return new Celebration("Merry Christmas");
+//    }
+
+//    Extension task 3
+//    public String newGreeting(@RequestParam(value = "message") String message){
+//        return String.format(message);
+//    }
 }
--- src/main/java/com/example/task_01/models/Celebration.java
+++ src/main/java/com/example/task_01/models/Celebration.java
@@ -17,6 +17,6 @@ public class Celebration {
     }
 
     public Celebration(){
-        
+
     }
 }
