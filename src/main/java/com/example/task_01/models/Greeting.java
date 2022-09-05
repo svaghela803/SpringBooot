d6d5534a338c554b7dae6159764ce27ba0c98227
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-05 14:53:52 +0100

    constructor for POJO

--- src/main/java/com/example/task_01/models/Greeting.java
+++ src/main/java/com/example/task_01/models/Greeting.java
@@ -5,5 +5,10 @@ public class Greeting {
     private String name;
     private String timeOfDay;
 
+    public Greeting(String name, String timeOfDay) {
+        this.name = name;
+        this.timeOfDay = timeOfDay;
+    }
+
     
 }
