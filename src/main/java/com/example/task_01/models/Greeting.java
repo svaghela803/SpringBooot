88c85e2476bf07ca20fc855360be84ea9a392ef2
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-05 14:54:45 +0100

    getters and setters for pojo

--- src/main/java/com/example/task_01/models/Greeting.java
+++ src/main/java/com/example/task_01/models/Greeting.java
@@ -10,5 +10,19 @@ public class Greeting {
         this.timeOfDay = timeOfDay;
     }
 
-    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    public void setTimeOfDay(String timeOfDay) {
+        this.timeOfDay = timeOfDay;
+    }
 }
